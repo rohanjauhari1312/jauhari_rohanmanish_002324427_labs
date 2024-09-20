f4669e3ba06147023dc1eede2a07cec6fb3db4f6
rohanjauhari <rohanjauhari@localhost>
2024-09-19 21:53:54 -0400

    Java swing UI for personal profile

---
+++ Assignment 1/src/model/Product.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author rohanjauhari
+ */
+public class Product {
+    String firstName;
+    String lastName;
+    String email;
+    String phone;
+    String dls;
+    String dln;
+    String ssn;
+    String al1;
+    String al2;
+    String city;
+    String state;
+    String zip;
+    String telephoneNumber;
+    String fax;
+    String nationality;
+    String race;
+    String gender;
+    String emergency;
+    String blood;
+    String employment;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    
+    public String getDriversLicenseState() {
+        return dls;
+    }
+    
+    public String getDriversLicenseNumber() {
+        return dln;
+    }
+    
+    public String getSocialSecurityNumber() {
+        return ssn;
+    }
+    
+    public String getAddressLine1() {
+        return al1;
+    }
+    
+    public String getAddressLine2() {
+        return al2;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+    
+    public String getState() {
+        return state;
+    }
+    
+    public String getZip() {
+        return zip;
+    }
+    
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+    
+    public String getFaxNumber() {
+        return fax;
+        
+    }    
+
+    public String getNationality() {
+        return nationality;
+    }
+    
+    public String getRace() {
+        return race;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    public String getEmergency() {
+        return emergency;
+    }
+    
+    public String getBlood() {
+        return blood;
+    }
+    
+    public String getEmployment() {
+        return employment;
+        
+    }
+    
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setDriversLicenseState(String dls) {
+        this.dls = dls;
+    }
+    
+    public void setDriversLicenseNumber(String dln) {
+        this.dln = dln;
+    }
+    
+    public void setSocialSecurityNumber(String ssn) {
+        this.ssn = ssn;
+    }
+    
+    public void setAddressLine1(String al1) {
+        this.al1 = al1;
+    }
+    
+    public void setAddressLine2(String al2) {
+        this.al2 = al2;
+    }
+    
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+    
+    public void setFaxNumber(String fax) {
+        this.fax = fax;
+    }
+    
+    public void setNationality(String nation) {
+        this.nationality = nation;
+    }
+    
+    public void setRace(String race) {
+        this.race = race;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+    
+    public void setEmergency(String emergency) {
+        this.emergency = emergency;
+    }
+    
+    public void setEmployment(String employment) {
+        this.employment = employment;
+    }
+    
+}
